a08395cc93f52f7192e4209e0aad85afc9c8c3ed
aaa <aaa@aaa.com>
2017-02-07 14:51:03 +0900

    Async

---
+++ 08_asyncTest/app/src/main/java/com/example/a/a08_asynctest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.a.a08_asynctest;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView myTextView;
+
+    class MyAsyncTask extends AsyncTask<Integer, Float, String> {
+
+        @Override
+        // 스레드에  해당하는 부분
+        protected String doInBackground(Integer... params) {
+            int i = params[0];
+            for(;i<100;i++){
+                Log.d("AsyncTask" , "count :"+ i);
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return "do in background done";
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myTextView = (TextView) findViewById(R.id.myTextView);
+        MyAsyncTask task = new MyAsyncTask();
+        task.execute(30);
+    }
+}
