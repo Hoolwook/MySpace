a43d08d91cb398a5fd2b39690a255ee26e76a0dd
aaa <aaa@aaa.com>
2017-02-09 11:53:03 +0900

    br battery cust

--- 16_br2/app/src/main/java/com/example/a/a16_br2/MainActivity.java
+++ 16_br2/app/src/main/java/com/example/a/a16_br2/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,21 +16,36 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int value= intent.getIntExtra("level",0);
-            Toast.makeText(context, "battery : " + value , Toast.LENGTH_SHORT).show();
+            String action = intent.getAction();
+            if(action.equals(intent.ACTION_BATTERY_CHANGED)){
+                int value= intent.getIntExtra("level",0);
+                Toast.makeText(context, "battery : " + value , Toast.LENGTH_SHORT).show();
+            }else if(action.equals(intent.ACTION_BATTERY_LOW)){
+                Toast.makeText(context, "battery is low" , Toast.LENGTH_SHORT).show();
+            }else if(action.equals("Nideren")){
+                Toast.makeText(context, "my broadcast", Toast.LENGTH_SHORT).show();
+            }
+
         }
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //z
+        
     }
 
     protected void onStart(){
         super.onStart();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        // 버튼의 인텐트를 받을 준비
+        filter.addAction("Nideren");
+        
+        
+        
+        
         registerReceiver(receiver,filter);
 
     }
@@ -40,4 +56,10 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(receiver);
 
     }
+    
+    public void onBtnClick(View v){         
+        Intent intent = new Intent("Nideren");
+        sendBroadcast(intent);
+    }
+    
 }
